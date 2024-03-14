d3c97b66ca4c19e68e459def395f898fb7933663
Shandiya Ravichandran <shandiyaravigmail.com>
2024-03-14 15:00:54 +0100

    changes made

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootswatch/dist/journal/bootstrap.min.css';
 import SubscribeProvider from './hooks/subscribeContext.jsx';
-import ThemeProvider from './hooks/themeContext.jsx';
 import FoodProvider from './hooks/foodContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <FoodProvider>
-        <SubscribeProvider>
-          <App />
-        </SubscribeProvider>
-      </FoodProvider>
-    </ThemeProvider>
+    <FoodProvider>
+      <SubscribeProvider>
+        <App />
+      </SubscribeProvider>
+    </FoodProvider>
   </React.StrictMode>
 );
